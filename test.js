192855cf512b99352834b46999e51e12dcb085c7
Kevin Martensson <kevinmartensson@gmail.com>
2017-09-23 03:16:25 +0200

    Use an array of macros to simplify tests

--- test.js
+++ test.js
@@ -1,88 +1,76 @@
-import test from 'ava';
+import {serial as test} from 'ava';
 import got from 'got';
 import micro from 'micro';
 import testListen from 'test-listen';
 import m from '.';
 
-const macro = async (t, opts, expected) => {
+const macro = async (t, {header, opts}, expected) => {
 	const access = m(opts);
 	const handler = () => ({});
 	const url = await testListen(micro(access(handler)));
-	t.is((await got(url, {json: true})).headers[opts.header], expected);
+	t.is((await got(url, {json: true})).headers[header], expected);
 };
 
-const envMacro = async (t, {env, header, value}, expected) => {
-	const ENV = process.env[env];
-	process.env[env] = value;
+const envMacro = async (t, {env, header}, expected) => {
+	const ENV = process.env[env.key];
+	process.env[env.key] = env.value;
 	const access = m();
 	const handler = () => ({});
 	const url = await testListen(micro(access(handler)));
 	t.is((await got(url, {json: true})).headers[header], expected);
-	process.env[env] = ENV;
+	process.env[env.key] = ENV;
 };
 
-test.serial('set allowCredentials', macro, {
-	allowCredentials: true,
-	header: 'access-control-allow-credentials'
+test('set allowCredentials', [macro, envMacro], {
+	opts: {allowCredentials: true},
+	header: 'access-control-allow-credentials',
+	env: {
+		key: 'ACCESS_ALLOW_CREDENTIALS',
+		value: '1'
+	}
 }, 'true');
 
-test.serial('omit setting allowCredentials if value is false', macro, {
-	allowCredentials: false,
-	header: 'access-control-allow-credentials'
+test('omit setting allowCredentials if value is falsy', [macro, envMacro], {
+	opts: {allowCredentials: false},
+	header: 'access-control-allow-credentials',
+	env: {
+		key: 'ACCESS_ALLOW_CREDENTIALS',
+		value: '0'
+	}
 }, undefined);
 
-test.serial('set allowCredentials using env variable', envMacro, {
-	env: 'ACCESS_ALLOW_CREDENTIALS',
-	value: '1',
-	header: 'access-control-allow-credentials'
-}, 'true');
-
-test.serial('omit setting allowCredentials if value is 0', envMacro, {
-	env: 'ACCESS_ALLOW_CREDENTIALS',
-	value: '0',
-	header: 'access-control-allow-credentials'
-}, undefined);
-
-test('set allowHeaders', macro, {
-	allowHeaders: ['Foo', 'Bar'],
-	header: 'access-control-allow-headers'
+test('set allowHeaders', [macro, envMacro], {
+	opts: {allowHeaders: ['Foo', 'Bar']},
+	header: 'access-control-allow-headers',
+	env: {
+		key: 'ACCESS_ALLOW_HEADERS',
+		value: 'Foo,Bar'
+	}
 }, 'Foo,Bar');
 
-test('set allowHeaders using env variable', envMacro, {
-	env: 'ACCESS_ALLOW_HEADERS',
-	value: 'Foo,Bar',
-	header: 'access-control-allow-headers'
-}, 'Foo,Bar');
-
-test('set allowMethods', macro, {
-	allowMethods: ['GET', 'POST'],
-	header: 'access-control-allow-methods'
+test('set allowMethods', [macro, envMacro], {
+	opts: {allowMethods: ['GET', 'POST']},
+	header: 'access-control-allow-methods',
+	env: {
+		key: 'ACCESS_ALLOW_METHODS',
+		value: 'GET,POST'
+	}
 }, 'GET,POST');
 
-test('set allowMethods using env variable', envMacro, {
-	env: 'ACCESS_ALLOW_METHODS',
-	value: 'GET,POST',
-	header: 'access-control-allow-methods'
-}, 'GET,POST');
-
-test('set maxAge', macro, {
-	maxAge: 1024,
-	header: 'access-control-max-age'
+test('set maxAge', [macro, envMacro], {
+	opts: {maxAge: 1024},
+	header: 'access-control-max-age',
+	env: {
+		key: 'ACCESS_MAX_AGE',
+		value: '1024'
+	}
 }, '1024');
 
-test('set maxAge using env variable', envMacro, {
-	env: 'ACCESS_MAX_AGE',
-	value: '1024',
-	header: 'access-control-max-age'
-}, '1024');
-
-test('set origin', macro, {
-	origin: '*',
-	header: 'access-control-allow-origin'
-}, '*');
-
-test('set origin using env variable', envMacro, {
-	env: 'ACCESS_ORIGIN',
-	value: '*',
-	header: 'access-control-allow-origin'
+test('set origin', [macro, envMacro], {
+	opts: {origin: '*'},
+	header: 'access-control-allow-origin',
+	env: {
+		key: 'ACCESS_ORIGIN',
+		value: '*'
+	}
 }, '*');
